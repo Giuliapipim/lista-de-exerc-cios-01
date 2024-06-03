99a4f8a9869df5d76b193510f65566c282c82e36
Giuliapipim <167253422+Giuliapipim@users.noreply.github.com>
2024-06-03 16:40:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var nome = "giulia";
+let idade = 17;
+console.log("Olá, meu nome é "+ nome + " e eu tenho" + idade +" anos");
+
+let num = prompt ("Digite um número:");
+let num2 = prompt ("Digite outro número:");
